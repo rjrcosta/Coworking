076bbe5595f37d4aca2f232a571ef2733dc4ec36
nfthiago <nfthiago@gmail.com>
2024-10-21 10:06:03 +0100

    Tabela ciadade, chave estrangeira com Edifício, faker edifício

---
+++ app/Models/Cidade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cidade extends Model
+{
+    /** @use HasFactory<\Database\Factories\CidadeFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'nome',
+    ];
+
+    public function edificios() {
+        return $this->belongsTo(Edificio::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,10 +7,13 @@ namespace Database\Seeders;
 * 18/10/2024
 */
 
+use App\Models\Cidade;
 use App\Models\Edificio;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,7 +23,30 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         //User::factory(5)->create();
+        //Cidade::factory(20)->create();
+        //Edificio::factory(50)->create();
 
+
+        $faker = Faker::create();
+
+        // // Definir os dados que irão compor o nome do edifício
+        $tipos = ['Edifício', 'Torre', 'Empreendimento'];
+        $posicao = ['Norte', 'Sul', 'Leste', 'Oeste'];
+
+
+        // Criar 50 edifícios
+        for ($i = 0; $i < 50; $i++) {
+            //Factory com os campos do Edifício
+            DB::table('edificios')->insert([
+            'nome' => $faker->randomElement($tipos) . ' ' . $faker->name() . ' ' . $faker->randomElement($posicao),
+            'morada' =>  fake()->address(),
+            'cod_cidade' =>  fake()->numberBetween(1, 20),
+            'cod_postal' => fake()->postcode(),
+            'contacto' => fake()->numerify("### ### ###"),
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]);
+        }
 
 //         User::factory()->create([
 //             'name' => 'Test User',
